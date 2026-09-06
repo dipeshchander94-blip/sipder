411e199218cf7915e8db81fdc877f9f04586ecc7
Dipesh <dipeshchander94@gmail.com>
2026-09-06 14:13:25 +0530

    Fix production settings

--- sipder/settings.py
+++ sipder/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-m*xb@+@ao6fkfhxr__5n7d@$=3vjz&ws&umwpl45+k)bk&efnj')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  False
 ALLOWED_HOSTS = [
     'sipder-1.onrender.com',
     'localhost',
