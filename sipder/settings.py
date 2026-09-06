2cc84dfafd26ef6ade661519308389ca6b7aaa11
Dipesh <dipeshchander94@gmail.com>
2026-09-06 13:50:09 +0530

    Secure Django secret key

--- sipder/settings.py
+++ sipder/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*xb@+@ao6fkfhxr__5n7d@$=3vjz&ws&umwpl45+k)bk&efnj'
+SECRET_KEY = os.environ.get('django-insecure-m*xb@+@ao6fkfhxr__5n7d@$=3vjz&ws&umwpl45+k)bk&efnj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
