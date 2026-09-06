aa9ee9977420f4c7f29d1b2fcce802eba68d5c3a
Dipesh <dipeshchander94@gmail.com>
2026-09-06 14:44:53 +0530

    Add Cloudinary storage for uploaded files

--- mj/models.py
+++ mj/models.py
@@ -1,20 +1,21 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models    
-class Employee(models.Model):  
-    id=models.CharField( max_length=20,primary_key=True)  
-    eid = models.CharField(max_length=20)    
-    ename = models.CharField(max_length=100)    
-    eemail = models.EmailField()    
-    econtact = models.CharField(max_length=15)    
-    #class Meta:    
-    #    fields = "__all__" 
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 
-from django.db import models  
-class UploadedFile(models.Model):  
-    title = models.CharField(max_length=100)  
-    file = models.FileField(upload_to='uploads/')  
-    def __str__(self):  
-        return self.title  
+class Employee(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    eid = models.CharField(max_length=20)
+    ename = models.CharField(max_length=100)
+    eemail = models.EmailField()
+    econtact = models.CharField(max_length=15)
+
+
+class UploadedFile(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(
+        upload_to='uploads/',
+        storage=MediaCloudinaryStorage()
+    )
+
+    def __str__(self):
+        return self.title
